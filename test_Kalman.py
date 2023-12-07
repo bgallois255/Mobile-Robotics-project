bc5a832737cc717b4ba4a5acb3b1cf56e55e29f3
RaphaelDssn <79713743+RaphaelDssn@users.noreply.github.com>
2023-12-07 20:44:27 +0100

    update test kalman

--- test_Kalman.py
+++ test_Kalman.py
@@ -9,7 +9,7 @@ dt = 1
 
 state_x = 0
 state_y = 0
-state_theta = math.pi / 4.0
+state_theta = math.pi / 2.0
 #state_theta = math.pi / 2.0
 state_vr = 0
 state_vl = 0
@@ -18,11 +18,13 @@ Kfilter = eKf.Kalman(state_x,state_y,state_theta)         # Attention, initialis
                                                           # sinon le filtre met bcp de temps à s'adapter à sa position réelle
 max_iter = 12
 max_iter2 = 12
+max_iter3 = 12
 #store real vs filtered position
-x1 = np.zeros(max_iter+max_iter2)
-y1 = np.zeros(max_iter+max_iter2)
-x2 = np.zeros(max_iter+max_iter2)
-y2 = np.zeros(max_iter+max_iter2)
+x1 = np.zeros(max_iter+max_iter2+max_iter3)
+y1 = np.zeros(max_iter+max_iter2+max_iter3)
+x2 = np.zeros(max_iter+max_iter2+max_iter3)
+y2 = np.zeros(max_iter+max_iter2+max_iter3)
+
 
 for iteration in range(max_iter):
     print(iteration)
@@ -34,10 +36,10 @@ for iteration in range(max_iter):
 
     Kfilter.dt_update(dt)
     Kfilter.measurement_wheels(state_vr, state_vl)
-    if(iteration % 8 == 0):
+    if(iteration % 6 == 0):
         Kfilter.measurement_position(state_x, state_y, state_theta)
-        state_vr = math.sqrt(2)
-        state_vl = math.sqrt(2)
+        state_vr = 1
+        state_vl = 1
         # state_vr = 1
         # state_vl = 1
         print("camera")
@@ -47,9 +49,9 @@ for iteration in range(max_iter):
     x2[iteration] = Kfilter.Mu[0][0]
     y2[iteration] = Kfilter.Mu[1][0]
 
-    state_x += 1
+    state_x += 0
     state_y += 1
-    state_theta = math.pi / 4.0
+    state_theta = math.pi / 2.0
 
 state_vr = 1
 state_vl = 1
@@ -67,7 +69,7 @@ for iteration in range(max_iter2):
 
     Kfilter.dt_update(dt)
     Kfilter.measurement_wheels(state_vr, state_vl)
-    if(iter % 8 == 0):
+    if(iter % 6 == 0):
         Kfilter.measurement_position(state_x, state_y, state_theta)
         # state_vr = math.sqrt(2)
         # state_vl = math.sqrt(2)
@@ -84,6 +86,38 @@ for iteration in range(max_iter2):
     state_y += 0
     state_theta = 0.0
 
+state_vr = 1
+state_vl = 1
+state_theta = -math.pi/2.0
+
+for iteration in range(max_iter3):
+    iter = iteration + 2*max_iter
+
+    print(iter)
+    print("Mu")  
+    print(Kfilter.Mu)
+    
+    x1[iter] = state_x
+    y1[iter] = state_y
+
+    Kfilter.dt_update(dt)
+    Kfilter.measurement_wheels(state_vr, state_vl)
+    if(iter % 6 == 0):
+        Kfilter.measurement_position(state_x, state_y, state_theta)
+        # state_vr = math.sqrt(2)
+        # state_vl = math.sqrt(2)
+        # state_vr = 1
+        # state_vl = 1
+        print("camera")
+
+    Kfilter.Kalman_filter()
+
+    x2[iter] = Kfilter.Mu[0][0]
+    y2[iter] = Kfilter.Mu[1][0]
+
+    state_x += 0
+    state_y -= 1
+    state_theta = -math.pi/2.0
 
 
 
